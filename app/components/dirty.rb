1a4cdb7028dee808637cf606f4169b966320218d
Juliasiberian <siberian.girl23@gmail.com>
2023-09-10 23:15:11 +0700

    added dirty (controllers and views)

---
+++ app/components/dirty.rb
@@ -0,0 +1,30 @@
+class Dirty
+
+    def initialize(city)
+        @city = city.downcase
+    end
+    
+    def call 
+        require "faraday"
+        response = Faraday.get(link)
+        
+        doc = Nokogiri::HTML(response.body)
+        
+        doc.search(".aqi-value__value").text
+    end  
+ 
+    def link
+        case @city
+        when "murmansk"
+            "https://www.iqair.com/ru/russia/#{@city}"
+        when "krasnoyarsk"
+            "https://www.iqair.com/ru/russia/krasnoyarsk-krai/#{@city}"
+        when "budapest"
+            "https://www.iqair.com/ru/hungary/central-hungary/#{@city}"
+
+        else 
+            "https://www.iqair.com/ru/russia/#{@city}"
+        end
+    end
+
+end
