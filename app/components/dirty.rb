61700e6739bcca981d61e176292942a2f23de2e7
Juliasiberian <siberian.girl23@gmail.com>
2023-09-11 20:19:53 +0700

    removing spaces

--- app/components/dirty.rb
+++ app/components/dirty.rb
@@ -1,30 +1,26 @@
+require "faraday"
+
 class Dirty
+  def initialize(city)
+    @city = city.downcase
+  end
+  
+  def call 
+    response = Faraday.get(link)
+    doc = Nokogiri::HTML(response.body)
+    doc.search(".aqi-value__value").text
+  end  
 
-    def initialize(city)
-        @city = city.downcase
+  def link
+    case @city
+    when "murmansk"
+      "https://www.iqair.com/ru/russia/#{@city}"
+    when "krasnoyarsk"
+      "https://www.iqair.com/ru/russia/krasnoyarsk-krai/#{@city}"
+    when "budapest"
+      "https://www.iqair.com/ru/hungary/central-hungary/#{@city}"
+    else 
+      "https://www.iqair.com/ru/russia/#{@city}"
     end
-    
-    def call 
-        require "faraday"
-        response = Faraday.get(link)
-        
-        doc = Nokogiri::HTML(response.body)
-        
-        doc.search(".aqi-value__value").text
-    end  
- 
-    def link
-        case @city
-        when "murmansk"
-            "https://www.iqair.com/ru/russia/#{@city}"
-        when "krasnoyarsk"
-            "https://www.iqair.com/ru/russia/krasnoyarsk-krai/#{@city}"
-        when "budapest"
-            "https://www.iqair.com/ru/hungary/central-hungary/#{@city}"
-
-        else 
-            "https://www.iqair.com/ru/russia/#{@city}"
-        end
-    end
-
+  end
 end
